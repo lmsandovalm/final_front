cae712599ad09efc0298f567a97ec0f70ad309a4
lmsandovalm <lmsandoval367@misena.edu.co>
2024-04-10 11:57:07 -0500

    ruta de api render

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,15 +13,15 @@ const Login = () => {
         e.preventDefault();
         
         try {
-            const response = await fetch('http://127.0.0.1:8000/api-auth/login/', {
+            const response = await fetch('https://backend-final1.onrender.com/api-auth/login/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                     
                 },
                 body: JSON.stringify({
-                    email: email,
-                    password: password,
+                    "email": email,
+                    "password": password,
                 }),
             });
 
