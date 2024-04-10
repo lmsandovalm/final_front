aa2c4a7cbf2b9e767b4e3dce20eaef4f9cc85849
lmsandovalm <lmsandoval367@misena.edu.co>
2024-04-10 11:29:37 -0500

    actualizacion api docker

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
         e.preventDefault();
         
         try {
-            const response = await fetch('https://localhost:8000/api-auth/login/', {
+            const response = await fetch('http://127.0.0.1:8000/api-auth/login/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
